0a2d30c3ee34efeb9bec6b58290efb6e9a6889ef
Prabh Chaudhary <prabh2012@hotmail.com>
2022-01-07 14:33:35 -0500

    first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '16d3d3eb57fe9b984f94';
+    this.client_secret = '520a7db61ae9b0d3e8308836070ae47a24c54b19';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
